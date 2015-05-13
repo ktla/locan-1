1c54b8de74249956152595a89aefe154b9df2c28
JP <JP@PET-PC>
2015-05-13 18:43:52 +0100

    Ajout de l'heure dans la classe DateFR

--- views/user/connexion.php
+++ views/user/connexion.php
@@ -1,5 +1,7 @@
 <div id="entete">
-
+    <div class="logo">
+        <img src="<?php echo SITE_ROOT . "public/img/wide_connexion.png" ?>" />
+    </div>
 </div>
 <div class="titre">
     Mes connexions
