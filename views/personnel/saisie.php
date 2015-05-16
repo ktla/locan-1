0042e3d6d0cf7780f370a32a1b40279c3d20cfd2
JP <JP@PET-PC>
2015-05-16 23:08:14 +0100

    Mise à jour du CSS pour la présentation des formulaires

--- views/personnel/saisie.php
+++ views/personnel/saisie.php
@@ -8,40 +8,40 @@
 <form action="<?php echo url("personnel", "saisie"); ?>" method="post" id="form-saisi-pers">
     <div class="page">
         <fieldset><legend> Identit&eacute;</legend>
-            <span class="text">
+            <span class="select" style="width: 150px;">
                 <label>Civilit&eacute;</label>
                 <?php echo $civilite; ?>
             </span>
-            <span class="text" style="width: 180px;">
+            <span class="text" style="width: 182px;">
                 <label> Nom </label>
                 <input type="text" name="nom" maxlength="30"  />
             </span>
-            <span class="text">
+            <span class="text" style="width: 150px">
                 <label> Pr&eacute;nom</label>
                 <input type="text" name="prenom" maxlength="30" />
             </span>
-            <span class="text">
+            <span class="text" style="width: 150px">
                 <label>Autre noms</label>
                 <input type="text" name="autrenom" maxlength="30" />
             </span>
-            <span class="text">
+            <span class="select" style="width: 150px;">
                 <label>Fonction</label>
                 <?php echo $functions; ?>
             </span>
-            <span class="text" style=" width: 356px;">
+            <span class="text" style=" width: 350px;">
                 <label>Grade</label>
                 <input type="text" class="grade" name="grade" maxlength="15" />
             </span>
-            <span class="date" style="position: relative;float: left;top: 13px;margin-bottom: 10px;">
+            <span class="text" style="width: 150px;">
                 <label>Date de naissance</label>
-                <div id="date"></div>
+                <div id="date" style="margin-top: 10px;"></div>
                 <input type="hidden" id="datenaiss" name="datenaiss" value="" />
             </span>
-            <span class="text" style="width: 142px; margin-right: 28px;">
+            <span class="text" style="width: 142px;margin-right: 20px;">
                 <label>T&eacute;l&eacute;phone</label>
                 <input type="text" name="telephone" maxlength="15"/>
             </span>
-            <span class="text" style="">
+            <span class="text" style="width: 150px;">
                 <label>Portable</label>
                 <input type="text" name="portable" maxlength="15" />
             </span>
